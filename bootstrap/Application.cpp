7f2632be1744bf718ee8c06ad646e8a0dd198fd7
Matt Dodd <matthewd@aie.edu.au>
2016-12-13 10:25:16 +1100

    Added call to SoundManager::update() in Application main loop to ensure that finished sound ID's get freed

--- bootstrap/Application.cpp
+++ bootstrap/Application.cpp
@@ -113,6 +113,7 @@ void Application::run(const char* title, int width, int height, bool fullscreen)
 			ImGui_NewFrame();
 
 			update(float(deltaTime));
+            SoundManager::getInstance()->update();
 			draw();
 
 			// draw IMGUI last
